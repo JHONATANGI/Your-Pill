29fee82674b8b5df53a80993a951db420ba7c9b7
Esteban Meza <estebanmezaher96@gmail.com>
2025-07-23 16:51:42 -0500

    Cambios en archivos y interfaces

--- backend/logout.php
+++ backend/logout.php
@@ -2,6 +2,6 @@
 session_start();        // Iniciamos la sesión (aunque sea para destruirla)
 session_unset();        // Limpiamos todas las variables de sesión
 session_destroy();      // Destruimos la sesión
-header("Location: ../index.html"); // Redirigimos al login u otra página
+header("Location: ../index.php"); // Redirigimos al login u otra página
 exit();
 ?>
